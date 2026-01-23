fb3880fefe6ce84cb1deaaf63674d8db9378a8f6
Fauzan Fiqriansyah <30542476+fauzanf07@users.noreply.github.com>
2026-01-23 16:46:47 +0700

    style : continue setting responsive design

--- resources/js/Pages/LandingPage.vue
+++ resources/js/Pages/LandingPage.vue
@@ -42,7 +42,7 @@ function loginWithGoogle() {
                 <div class="w-[70dvw]  flex items-center gap-10 pr-10 phone:flex-col phone:pr-3 phone:gap-5  phone:w-[90dvw]">
                     <img src="/storage/images/akunthink-illustration.jpg" width="650" class="phone:!w-[100%]"/>
                     <div class=" flex flex-col gap-10 phone:items-center ">
-                        <span class="text-[#80da5a] text-6xl phone:text-2xl text-center">Tak perlu bayar software akuntansi mahal, kalau pakai Excel* juga bisa</span>
+                        <span class="text-[#80da5a] text-6xl phone:text-3xl text-center">Tak perlu bayar software akuntansi mahal, kalau pakai Excel* juga bisa</span>
                         <Button label="Mulai Coba" pt:root:class="w-64 bg-[#eb8227] border-[#eb8227] text-xl py-3" @click="loginWithGoogle"/>
                     </div>
                 </div>
@@ -67,25 +67,25 @@ function loginWithGoogle() {
                             <h1 class="text-5xl leading-[3.5rem] phone:text-4xl">3 alasan AkunThink paling pas untuk bisnis kecil !! </h1>
                         </div>
                         <div class="flex flex-row gap-12 w-full justify-center phone:flex-col">
-                            <div class="flex flex-col phone:border phone:border-black phone:p-3 phone:rounded-md">
+                            <div class="flex flex-col phone:border phone:border-black phone:py-7 phone:rounded-md">
                                 <div class="flex justify-center">
                                     <div class="flex justify-center items-center w-28 h-28 border-8 rounded-full border-black">
                                         <span class="text-6xl font-bold">Rp</span>
                                     </div>
                                 </div>
-                                <h3 class="text-3xl mt-5">Murah dibanding software lainnya </h3>
-                                <span class="text-xl mt-3">Tak perlu bayar jutaan per tahun untuk berlangganan software akuntansi</span>
+                                <h3 class="text-3xl mt-5 phone:text-center">Murah dibanding software lainnya </h3>
+                                <span class="text-xl mt-3 phone:text-center">Tak perlu bayar jutaan per tahun untuk berlangganan software akuntansi</span>
                             </div>
-                            <div class="flex flex-col phone:border phone:border-black phone:p-3 phone:rounded-md">
+                            <div class="flex flex-col phone:border phone:border-black phone:py-7 phone:rounded-md">
                                 <div class="flex justify-center">
                                     <div class="flex justify-center items-center w-28 h-28 border-8 rounded-full border-black">
                                         <i class="pi pi-google text-6xl font-bold"/>
                                     </div>
                                 </div>
-                                <h3 class="text-3xl mt-5">Berbasis Platform Google Workspace </h3>
-                                <span class="text-xl mt-3">Jaminan keandalan dalam bekerja dan menyimpan data online</span>
+                                <h3 class="text-3xl mt-5 phone:text-center">Berbasis Platform Google Workspace </h3>
+                                <span class="text-xl mt-3 phone:text-center">Jaminan keandalan dalam bekerja dan menyimpan data online</span>
                             </div>
-                            <div class="flex flex-col pl-6 phone:border phone:border-black phone:p-3 phone:rounded-md">
+                            <div class="flex flex-col pl-6 phone:border phone:border-black phone:py-7 phone:pl-0 phone:rounded-md">
                                 <div class="flex justify-center">
                                     <div class="flex justify-center items-center w-28 h-28 border-8 rounded-full border-black">
                                         <svg
@@ -104,17 +104,17 @@ function loginWithGoogle() {
         
                                     </div>
                                 </div>
-                                <h3 class="text-3xl mt-5">Metode Input Transaksi Termudah </h3>
-                                <span class="text-xl mt-3 ">Dibuat dengan metode single entry modified yang mengadaptasi Jurnal Kas</span>
+                                <h3 class="text-3xl mt-5 phone:text-center">Metode Input Transaksi Termudah </h3>
+                                <span class="text-xl mt-3 phone:text-center">Dibuat dengan metode single entry modified yang mengadaptasi Jurnal Kas</span>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="w-[70dvw]  flex items-center gap-10 mt-10 px-10">
-                    <div class="grid grid-cols-3 w-full gap-7">
-                        <div class="flex flex-col min-h-[30rem] justify-between ">
+                <div class="w-[70dvw]  flex items-center gap-10 mt-10 px-10 phone:w-[100dvw]">
+                    <div class="grid grid-cols-3 w-full gap-7 phone:flex phone:flex-col">
+                        <div class="flex flex-col min-h-[30rem] justify-between phone:flex-row phone:min-h-0 phone:items-center">
                             <h1 class="text-5xl text-[#eb8227] font-bold">Mulai <br/>saja dulu...</h1>
-                            <img src="/storage/images/akunthink-logo.png" width="200" class="ml-4"/>
+                            <img src="/storage/images/akunthink-logo.png" width="200" class="ml-4 phone:w-32"/>
                         </div>
                         <div class="flex flex-col gap-5 items-center">
                             <div class="flex flex-col bg-[#7ed957] items-center rounded-xl py-7 px-5 min-h-[28rem]">
@@ -159,35 +159,35 @@ function loginWithGoogle() {
                     </div>
                 </div>
                 <div class="w-full  flex  flex-col justify-center items-center gap-10  bg-[#7ed957] py-12 mt-14">
-                    <div class="w-[70dvw] flex  flex-col justify-center  gap-10 px-10 items-center">
+                    <div class="w-[70dvw] flex  flex-col justify-center  gap-10 px-10 items-center phone:w-[85dvw]">
                         <h1 class="text-4xl text-white font-bold w-[30rem] text-center">Kata mereka yang sudah menggunakan AkunThink</h1>
-                        <div class="grid grid-cols-3 w-full gap-7 mt-24">
-                            <div class="flex flex-col gap-4">
+                        <div class="grid grid-cols-3 w-full gap-7 mt-24 phone:flex phone:flex-col phone:mt-7">
+                            <div class="flex flex-col gap-4 phone:border phone:border-white phone:p-4 phone:rounded-md">
                                 <div class="flex gap-3 items-end">
                                     <div class="w-20 h-20 rounded-3xl bg-[url(/storage/images/sidonikmat.jpg)] bg-cover"></div>
                                     <h1 class="text-xl font-extrabold text-white">Bakmi Jawa<br/> Sido Nikmat</h1>
                                 </div>
-                                <p class="text-white">Ternyata mengerjakan keuangan di sela-sela kesibukan mengurusi operasional kedai, sangat bisa lho! Terimakasih  untuk AkunThink yang sudah membantu kami.</p>
+                                <p class="text-white phone:text-justify">Ternyata mengerjakan keuangan di sela-sela kesibukan mengurusi operasional kedai, sangat bisa lho! Terimakasih  untuk AkunThink yang sudah membantu kami.</p>
                             </div>
-                            <div class="flex flex-col gap-4">
+                            <div class="flex flex-col gap-4 phone:border phone:border-white phone:p-4 phone:rounded-md">
                                 <div class="flex gap-3 items-end">
                                     <div class="w-20 h-20 rounded-3xl bg-[url(/storage/images/rendang-sapi-uni-vivi.jpg)] bg-cover bg-center"></div>
                                     <h1 class="text-xl font-extrabold text-white">Rendang Sapi<br/> Uni Vivi</h1>
                                 </div>
-                                <p class="text-white">Produk kami biasanya dipesan pas ada event seperti arisan dan acara keluarga. Kami butuhnya aplikasi akuntansi yang mudah dan murah. Untung ada AkunThink!</p>
+                                <p class="text-white phone:text-justify">Produk kami biasanya dipesan pas ada event seperti arisan dan acara keluarga. Kami butuhnya aplikasi akuntansi yang mudah dan murah. Untung ada AkunThink!</p>
                             </div>
-                            <div class="flex flex-col gap-4">
+                            <div class="flex flex-col gap-4 phone:border phone:border-white phone:p-4 phone:rounded-md">
                                 <div class="flex gap-3 items-end">
                                     <div class="w-20 h-20 rounded-3xl bg-[url(/storage/images/sambal-gledek.jpg)] bg-cover bg-center"></div>
                                     <h1 class="text-xl font-extrabold text-white">Sambal Gledeg<br/> BVJ Original</h1>
                                 </div>
-                                <p class="text-white">Buat kami yang berjualan online, AkunThink sangat membantu dalam mencatat transaksi penjualan dan pembelian bahan baku. Simpel dan mudah dipahami. </p>
+                                <p class="text-white phone:text-justify">Buat kami yang berjualan online, AkunThink sangat membantu dalam mencatat transaksi penjualan dan pembelian bahan baku. Simpel dan mudah dipahami. </p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div class="w-[70dvw]  flex items-center gap-10 px-10 py-10">
-                    <div class="grid grid-cols-2 w-full gap-7">
+                    <div class="grid grid-cols-2 w-full gap-7 phone:flex phone:flex-col">
                         <div class="flex flex-col min-h-[30rem] justify-between ">
                             <h1 class="text-5xl text-[#eb8227] font-bold leading-[3.5rem]">Jangan ragu <br/>kontak kami!</h1>
                             <img src="/storage/images/akunthink-logo.png" width="200" class="ml-4"/>
